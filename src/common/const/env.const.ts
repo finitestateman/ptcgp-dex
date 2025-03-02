bab29646e8c01ce017310231676436c9e0d44885
Finite Stateman <finitestateman@gmail.com>
2025-03-02 13:03:08 +0900

    feat: Sync PostgreSQL with TypeORM
    - Add .env validation using Joi
    - Support hierarchial .env keys

---
+++ src/common/const/env.const.ts
@@ -0,0 +1,10 @@
+export const DOTENV = {
+    DB: {
+        TYPE: 'DB-TYPE',
+        HOST: 'DB-HOST',
+        PORT: 'DB-PORT',
+        USERNAME: 'DB-USERNAME',
+        PASSWORD: 'DB-PASSWORD',
+        DATABASE: 'DB-DATABASE',
+    },
+} as const;
